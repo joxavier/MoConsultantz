fae72e58c85e168adf8e7b74acd4712a29e587af
joxavier <35542263+joxavier@users.noreply.github.com>
2024-01-23 14:43:17 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,6 @@ const StatCard = ({
 };
 
 export default function Home() {
-
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24 font"
@@ -92,8 +91,7 @@ export default function Home() {
       </div>
 
       <div className={`mb-8 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-8`}
-        style={{ opacity: animationTriggered ? 1 : 0, transform: animationTriggered ? 'translateY(0)' : 'translateY(20px)', transition: 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out', margin: '20px 0' }}>
-        {/* Item 1 */}
+        style={{ transition: 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out', margin: '20px 0' }}>
         <StatCard
           link="#education"
           iconSrc="icons/Award.svg"
@@ -101,14 +99,14 @@ export default function Home() {
           title="2+"
           description="Degrees"
         />
-                <StatCard
+        <StatCard
           link="#work"
           iconSrc="icons/Target.svg"
           altText="Experienced Web Developer"
           title="9+"
           description="Years of Experience"
         />
-                <StatCard
+        <StatCard
           link="#reviews"
           iconSrc="icons/Customer.svg"
           altText="Top Rated Web Developer"
@@ -116,57 +114,6 @@ export default function Home() {
           description="Satisfied Clients"
         />
       </div>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Docs{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Learn{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Templates{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Explore starter templates for Next.js.
-            </p>
-          </a>
-
     </main>
 
   );
