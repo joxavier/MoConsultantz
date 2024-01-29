9030b56bf61e990fa8c5fefaeacb22c1e3288902
joxavier <35542263+joxavier@users.noreply.github.com>
2024-01-29 14:26:49 -0500

    work icon sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,8 +111,8 @@ const QualificationsIcon = ({
             src={iconSrc}
             alt={altText}
             style={{
-              width: '100px',
-              height: '100px',
+              width: '110px',
+              height: '110px',
               objectFit: 'contain',
             }} />
         </div>
@@ -429,16 +429,17 @@ export default function Home() {
 
           <h1>Experiences</h1>
           <p style={{ fontFamily: 'Arial, sans-serif', fontSize: '16px', lineHeight: '1.5' }}>My years of experience spans many organizations</p>
-          <ul className="skills mt-4 mb-8 grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-5 gap-4" style={{}}>
-            <QualificationsIcon title="XavierAcademy" iconSrc="qualifications/XavierAcademy.svg" altText="XavierAcademy Logo" />
-            <QualificationsIcon title="Metaparlour" iconSrc="https://metaparlour.io/metaparlour.svg" altText="Metaparlour Logo" />
-            <QualificationsIcon title="LA Fitness" iconSrc="qualifications/LAFitness.png" altText="LA Fitness Logo" />
-            <QualificationsIcon title="NCR" iconSrc="qualifications/NCR.png" altText="NCR Logo" />
-            <QualificationsIcon title="Manulife" iconSrc="qualifications/manulife.png" altText="Manulife Logo" />
-            <QualificationsIcon title="Nissan" iconSrc="qualifications/nissan.jpg" altText="Nissan Logo" />
-            <QualificationsIcon title="Best Buy" iconSrc="qualifications/bestbuy.jpg" altText="BestBuy Logo" />
-          </ul>
-
+          <div style={{ display: 'flex', width: 'fit-content', margin: '0 auto' }}>
+            <ul className="skills mt-4 mb-8 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-4" style={{}}>
+              <QualificationsIcon title="XavierAcademy" iconSrc="qualifications/XavierAcademy.svg" altText="XavierAcademy Logo" />
+              <QualificationsIcon title="Metaparlour" iconSrc="https://metaparlour.io/metaparlour.svg" altText="Metaparlour Logo" />
+              <QualificationsIcon title="LA Fitness" iconSrc="qualifications/LAFitness.png" altText="LA Fitness Logo" />
+              <QualificationsIcon title="NCR" iconSrc="qualifications/NCR.png" altText="NCR Logo" />
+              <QualificationsIcon title="Manulife" iconSrc="qualifications/manulife.png" altText="Manulife Logo" />
+              <QualificationsIcon title="Nissan" iconSrc="qualifications/nissan.jpg" altText="Nissan Logo" />
+              <QualificationsIcon title="Best Buy" iconSrc="qualifications/bestbuy.jpg" altText="BestBuy Logo" />
+            </ul>
+          </div>
         </div>
 
         <div className="contact" id='contact'
@@ -475,11 +476,8 @@ export default function Home() {
               </a>
             ))}
           </div>
-
         </div>
-
       </div >
-
     </main >
 
   );
