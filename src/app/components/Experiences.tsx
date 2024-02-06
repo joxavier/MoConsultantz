aa699986342aeff206f3b504be4bf8ab7286129d
joxavier <35542263+joxavier@users.noreply.github.com>
2024-02-05 19:38:53 -0500

    fix

--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -12,13 +12,13 @@ const ExperiencesSection: React.FC = () => {
       </p>
       <div style={{ display: 'flex', width: 'fit-content', margin: '0 auto' }}>
         <ul className="skills mt-4 mb-8 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-4">
-          <QualificationsIcon title="XavierAcademy" iconSrc="qualifications/XavierAcademy.svg" altText="XavierAcademy Logo" />
+          <QualificationsIcon title="XavierAcademy" iconSrc="/qualifications/XavierAcademy.svg" altText="XavierAcademy Logo" />
           <QualificationsIcon title="Metaparlour" iconSrc="https://metaparlour.io/metaparlour.svg" altText="Metaparlour Logo" />
-          <QualificationsIcon title="LA Fitness" iconSrc="qualifications/LAFitness.png" altText="LA Fitness Logo" />
-          <QualificationsIcon title="NCR" iconSrc="qualifications/NCR.png" altText="NCR Logo" />
-          <QualificationsIcon title="Manulife" iconSrc="qualifications/manulife.png" altText="Manulife Logo" />
-          <QualificationsIcon title="Nissan" iconSrc="qualifications/nissan.jpg" altText="Nissan Logo" />
-          <QualificationsIcon title="Best Buy" iconSrc="qualifications/bestbuy.jpg" altText="BestBuy Logo" />
+          <QualificationsIcon title="LA Fitness" iconSrc="/qualifications/LAFitness.png" altText="LA Fitness Logo" />
+          <QualificationsIcon title="NCR" iconSrc="/qualifications/NCR.png" altText="NCR Logo" />
+          <QualificationsIcon title="Manulife" iconSrc="/qualifications/manulife.png" altText="Manulife Logo" />
+          <QualificationsIcon title="Nissan" iconSrc="/qualifications/nissan.jpg" altText="Nissan Logo" />
+          <QualificationsIcon title="Best Buy" iconSrc="/qualifications/bestbuy.jpg" altText="BestBuy Logo" />
         </ul>
       </div>
     </div>
