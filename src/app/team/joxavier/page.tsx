a347575e7c2227f3aae0e9dc10d8622bac851bbf
joxavier <35542263+joxavier@users.noreply.github.com>
2024-04-20 22:51:36 -0400

    bio

--- src/app/team/joxavier/page.tsx
+++ src/app/team/joxavier/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       style={{ fontFamily: 'Anton, sans-serif' }}>
       <Header />
 
-      <div style={{ maxWidth: '1320px'}}>
+      <div style={{ maxWidth: '1320px' }}>
 
 
         <Hero {...hero} />
@@ -45,13 +45,11 @@ export default function Home() {
           <div className={"grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4"}>
             <div className="bio sm:order-first lg:order-last">
               <h1 style={{ color: 'rgb(28, 78, 110)', backgroundColor: 'dark' ? 'white' : 'none', marginBottom: 4 }}>About Me</h1>
-                <p style={{ fontFamily: 'Arial, sans-serif', fontSize: '16px', lineHeight: '1.5' }}>
-                By the age of 21, I had already completed my second undergraduate degree and amassed over $100,000 through various jobs. Despite this early success, I found myself at a crossroads upon graduating with dual degrees in Computer Science and Business. While many would have pursued lucrative careers with big tech companies, I was drawn to the rapidly evolving landscape of technology, particularly blockchain, and the unprecedented opportunities presented by the COVID-19 pandemic, which accelerated internet adoption worldwide.
-                <br />
-                Rather than opting for the conventional path, I felt a strong pull to give back to the community that shaped me. Instead of chasing corporate success, I chose to use my skills and resources to empower small businesses to thrive in the digital age. It was a decision rooted in my belief in the transformative power of technology and my desire to make a tangible impact where it mattered most.
-                <br />
-                On September 24th, 2023, Mo was officially incorporated in Canada. In the words of the late Nipsey Hussle, "The marathon continues." This journey is not just about reaching the finish line but about embracing the challenges, staying committed to the cause, and leaving a lasting legacy that uplifts and empowers others.
-                </p>
+              <p style={{ fontFamily: 'Arial, sans-serif', fontSize: '16px', lineHeight: '1.5' }}>
+                At 21, I was completing my second undergraduate degree and had already made my first $100,000 from various jobs. Despite initial success, I chose a different path after graduating with dual degrees in Computer Science and Business. Instead of pursuing corporate roles, I focused on empowering small businesses in the digital era. Motivated by technology's transformative potential and the COVID-19 pandemic's impact, I founded Mo on September 24th, 2023. As Nipsey Hussle said, 'The marathon continues.' My journey is about embracing challenges, making a lasting impact, and uplifting others.
+                <br /><br />
+              </p>
+            </div>
             <div className="social">
               <Feed imageData={socialData}></Feed>
             </div>
