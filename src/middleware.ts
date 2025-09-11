8e25d5bb6fc6d5076364d474a0db0fcdbb82e032
joxavier <35542263+joxavier@users.noreply.github.com>
2025-09-11 12:35:51 -0400

    services router

--- src/middleware.ts
+++ src/middleware.ts
@@ -70,8 +70,8 @@ export default async function middleware(req: NextRequest) {
 
   if (hostname === "localhost:3000" || hostname === "moconsultantz.com") {
     console.log(path)
-    if (path.startsWith("/devz") || path.startsWith("/vestmentz") || path.startsWith("/consultantz") || path.startsWith("/feed") || path.startsWith("/moos") || path.startsWith("/josh")) {
-      console.log(path)
+    if (path.startsWith("/devz") || path.startsWith("/vestmentz") || path.startsWith("/consultantz") || path.startsWith("/feed") || path.startsWith("/moos") || path.startsWith("/josh") || path.startsWith("/services")) {
+      console.log("cechk" + path)
       return NextResponse.rewrite(new URL(`${path}`, req.url));
     }
     else{
@@ -80,7 +80,7 @@ export default async function middleware(req: NextRequest) {
   }
 
   if (hostname === "modevz.ca" || hostname === "modevz.com") {
-    if (path.startsWith("/devz") || path.startsWith("/vestmentz") || path.startsWith("/consultantz") || path.startsWith("/feed") || path.startsWith("/moos")) {
+    if (path.startsWith("/devz") || path.startsWith("/vestmentz") || path.startsWith("/consultantz") || path.startsWith("/feed") || path.startsWith("/moos") || path.startsWith("/services")) {
       console.log(path)
       return NextResponse.rewrite(new URL(`${path}`, req.url));
     }
