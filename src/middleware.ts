e44b355d3c311442acbb2357cdf750f2ddfeee5d
joxavier <35542263+joxavier@users.noreply.github.com>
2025-11-28 12:07:52 -0500

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -95,7 +95,7 @@ export default async function middleware(req: NextRequest) {
     if (path.startsWith("/articles")) {
       return NextResponse.rewrite(new URL(`/feed/0${path}`, req.url));
     }
-    if (path.startsWith("/about")) {
+    if (path.startsWith("/about") || path.startsWith("/newsletter")) {
       return NextResponse.rewrite(new URL(`/consultantz${path}`, req.url));
     }
     else {
