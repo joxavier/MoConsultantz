e4e0f05f4b432aaa0260b7b21e6dd8d72ca96a89
joxavier <35542263+joxavier@users.noreply.github.com>
2026-01-16 11:54:19 -0500

    fix

--- src/app/vestmentz/mocoin/strategicReserve/page.tsx
+++ src/app/vestmentz/mocoin/strategicReserve/page.tsx
@@ -332,7 +332,7 @@ export default function StrategicReserve() {
                     color: '#fff',
                     padding: '4px 0'
                   }}
-                  formatter={(value: any, name: string, entry: any) => {
+                  formatter={(value: any, name: any, entry: any) => {
                     return [
                       <div key="tooltip-content" style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
                         <span style={{ color: entry.payload.color, fontWeight: 'bold', fontSize: '14px' }}>
